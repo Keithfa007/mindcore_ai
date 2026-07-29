b29a3bdc0c4b204a619de81df8202ee1678dd986
Keithfa007 <93363064+Keithfa007@users.noreply.github.com>
2026-07-29 07:09:55 +0200

    Remove dotenv/.env loading from startup (no longer needed; keys are in the proxy)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -50,11 +49,6 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SettingsService.init();
-  try {
-    await dotenv.load(fileName: 'assets/config/env/.env');
-  } catch (_) {
-    try { await dotenv.load(fileName: '.env'); } catch (_) {}
-  }
   await OpenAiTtsService.instance.init();
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp, DeviceOrientation.portraitDown,
